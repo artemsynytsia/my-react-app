c31e743b2313d050086b1820ad565d2e6d72bca4
artemsynytsia <synartem2023@gamil.com>
2025-05-10 19:17:53 +0300

    initialize git repozytory

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { FirstPage } from "./components/FirstPages/mainText";
+
+function App() {
+  return (
+    <FirstPage
+      title="СЕЛЕКЦІЯ ПОСЛУГ"
+      subtitle="  ПЕРСОНАЛЬНА СТИЛІСТКА КРІСТІНА ДУТЧАК"
+    />
+  );
+}
+
+export default App;
---
+++ src/components/FirstPages/mainText.tsx
@@ -0,0 +1,14 @@
+export const FirstPage = ({
+  title,
+  subtitle,
+}: {
+  title: string;
+  subtitle: string;
+}) => {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h2>{subtitle}</h2>
+    </div>
+  );
+};
